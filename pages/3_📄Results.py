0d575e9dd389c35cd22ad1875a6c5247fced59f3
SarahMBradford <145512131+SarahMBradford@users.noreply.github.com>
2023-12-06 10:30:21 -0500

    Update 3_📄Results.py

--- pages/3_📄Results.py
+++ pages/3_📄Results.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
 st.title("Results")
-st.image(Image.open('/Users/sarahbradford/Downloads/tea_please.jpg'), use_column_width=True)
+st.image(Image.open('tea_please.jpg'), use_column_width=True)
 st.write("By using all features in the Starbucks dataset, it can be concluded that the XGBoost model is the best model for predicting whether a drink is tea or not. The XGBoost model had an accuracy of 0.99, which is the highest accuracy out of all the models. The XGBoost model also had the highest precision, recall, and F1 score.")
 st.write("Go ahead and order that tea! Now that you have have some class!")
