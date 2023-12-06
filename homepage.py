77d7383dadf59eea9567c6c06ede96b1a666a4ac
SarahMBradford <145512131+SarahMBradford@users.noreply.github.com>
2023-12-06 13:37:18 -0500

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -32,22 +32,3 @@ st.sidebar.success("Page Navigation")
 st.write("Welcome to the Starbucks Tea Classification App! This app will enable you to easily view the various tea drinks that Starbucks has to offer, if one day you are trying to decide whether you should order tea or coffee! The data used in this app is from the Starbucks Menu dataset on Kaggle. The data was collected in 2017 and contains nutritional information on Starbucks' food and drink menu. The dataset has 242 rows and 18 columns. Additionally, the data contains a total of 9 beverage categories. Out of the 9, roughly 78%, while the remaining 22% of the beverages are tea-based. ")
 st.subheader("Why Tea?")
 st.write("Tea is an excellent alternative to coffee. Tea has several health benefits, including reducing the risk of strokes and heart attacks while boosting the immune system and simultaneously reducing the risk of cancer. Tea contains less caffeine than coffee, so it is an ideal alternative for those who get overstimulated. Lastly, tea is also a great option for people who desire to reduce their sugar and fat intake, as many tea drinks are naturally sweet and do not require added sugar or creamers.")
-sorted_starbucks = pd.read_csv("sorted_starbucks.csv")
-tazo_teas = tazo_teas = sorted_starbucks[189:241]
-t_sug_cal = (
-    alt.Chart(tazo_teas)
-    .mark_circle(size=200)
-    .encode(
-        x='Sugars (g):Q',
-        y='Calories:Q',
-        size='Caffeine (mg):Q',
-        color=alt.condition(single, 'Beverage:N', alt.value('lightgray')),
-        tooltip=['Sugars (g):Q', 'Calories:Q', 'Caffeine (mg):Q', 'Beverage:N']
-    )
-    .interactive()
-    .add_selection(single)
-)
-st.subheader("Relationships between Caffienated Tazo Teas with Sugars and Calories")
-st.altair_chart(t_sug_cal, use_container_width=True)
-st.markdown('<p class="font_subtext">Figure 3: Caffienated Tazo Teas with Sugars and Calories', unsafe_allow_html=True) 
-
